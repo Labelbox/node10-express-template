cd0cece272a13e2c1422b01c0a844642b03dc40e
Connor Harwood <harwood.connor@gmail.com>
2020-02-04 23:17:46 -0800

    add node12 templates

---
+++ template/node12-express/index.js
@@ -0,0 +1,25 @@
+// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+"use strict"
+
+const express = require('express')
+const app = express()
+const handler = require('./function/handler');
+const bodyParser = require('body-parser')
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({
+	limit: '50mb'
+}));
+app.use(bodyParser.raw());
+app.use(bodyParser.text({ type : "text/*" }));
+app.disable('x-powered-by');
+
+app.use((req, res) => handler(req, res));
+
+const port = process.env.http_port || 3000;
+
+app.listen(port, () => {
+    console.log(`OpenFaaS Node.js listening on port: ${port}`)
+});
